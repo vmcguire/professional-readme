44906e67aa31aec9964a8945cd4d6af784028e99
vmcguire <victor.mcguire@gmail.com>
2021-02-14 15:52:45 -0800

    final commit

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,87 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const markdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const questions = [ 
+    {
+      name: "title",
+      type: "input",
+      message: "What is the Title of Your Project?"
+    },
+    {
+      name: "description",
+      type: "input",
+      message: "Please describe your Project."
+    },
+    {
+      name: "installation",
+      type: "input",
+      message: "Please provide the Installation Instructions."
+    },
+    {
+        name: "usage",
+        type: "input",
+        message: "Please provide the Usage Instructions."
+      },
+      {
+        name: "contribution",
+        type: "input",
+        message: "Please provide the Contributing Guidelines."
+      },
+      {
+        name: "test",
+        type: "input",
+        message: "Please provide the Test Instructions."
+      },
+      {
+        name: "license",
+        type: "list",
+        message: "Please select the license for this project.",
+        choices: ['MIT License', 'Open Data Commons Attrubution License', 'Apache 2.0 License']
+      },
+      {
+        name: "copyright_name",
+        type: "input",
+        message: "What name do you want to use for your copyright?"
+      },
+      {
+        name: "copyright_year",
+        type: "input",
+        message: "What year do you want to use for your copyright?"
+      },
+      {
+        name: "github_username",
+        type: "input",
+        message: "What is your GitHub Username?"
+      },
+      {
+        name: "email",
+        type: "input",
+        message: "What is your email for further questions?"
+      }
+  ];
 
 // TODO: Create a function to initialize app
-function init() {}
+
+function init() {
+    inquirer
+    .prompt(questions)
+    .then(data => {  
+    const filename =
+    data.title
+    .toLowerCase()
+    .split(' ')
+    .join('')+'.md';
+    // TODO: Create a function to write README file
+    fs.writeFile(filename, markdown(data), function(err) {
+      if (err) {
+        return console.log(err);
+      }
+    });
+  });
+}
 
 // Function call to initialize app
 init();
---
+++ Develop/week9project.md
@@ -0,0 +1,43 @@
+
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+# Week 9 Project
+
+## Table of Contents
+1. [Description](#Description)
+2. [Installation Instructions](#Installation-Instructions)
+3. [Usage Instructions](#Usage-Instructions)
+4. [Contribution Guidelines](#Contribution-Guidelines)
+5. [Test Instructions](#Test-Instructions)
+6. [Questions](#Questions)
+7. [License](#License)
+
+## Description 
+Work as part of Coding Bootcamp
+
+## Installation Instructions
+Install simply.
+
+## Usage Instructions
+Use often.
+
+## Contribution Guidelines
+Contribute and give credit.
+
+## Test Instructions
+Test when necessary.
+
+## Questions
+Please visit my repo here for any further questions: <https://github.com/vmcguire>
+
+My email address is: <victor.mcguire@gmail.com>
+
+## License
+Copyright 2021 Victor McGuire
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+    
+    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
